e6b001b3885e8e81c9864e5306a0d3f67dde1e58
alexitaylor <alexi@bluesunrise.com>
2017-06-08 19:34:30 -0700

    Added some transitions and routes
    Need to iron out animation and transitions.
    Need to add Bio component.

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -6,7 +6,8 @@
           <div id="app">
             <div class="row">
               <div class="col s12 m12 l12">
-                <div id="moon">
+              <div v-if="isProjects">
+                <div id="moon" v-bind:class="{ 'spin-in': isSpin, show: isSlideOut }" v-on:click="isProjects = !isProjects">
                   <div class="content">
                     <section id="intro">
                       <section id="name">
@@ -28,12 +29,17 @@
                           <a target="_blank" href="https://www.linkedin.com/in/alexitaylor/"><i class="fa fa-linkedin fa-size" aria-hidden="true"></i></a>
                         </section>
                       </div>
-                      <div class="row">
-                        <button class="btnRandom">LEARN MORE</button>
-                      </div>
                     </section>
+                    <div class="row">
+                      <button class="btnRandom" v-on:click="isSlideOut = !isSlideOut">LEARN MORE</button>
+                    </div>
                   </div>
                 </div>
+                </div>
+                <div v-else>
+                <router-link class="links" to="/Projects"><button class="btnRandom" v-bind:class="{ 'hide-projects': isProjects }" v-on:click="isSlideOut = !isSlideOut">Projects</button></router-link>
+                <router-link class="links" to="/Bio"><button class="btnRandom" v-bind:class="{ 'hide-projects': isProjects }" v-on:click="isSlideOut = !isSlideOut">Bio</button></router-link>
+              </div>
               </div>
             </div>
           </div>
@@ -49,55 +55,24 @@ export default {
   data() {
     return {
       msg: 'Welcome to Your Vue.js App',
+      isSlideOut: false,
+      isSpin: true,
+      isProjects: true,
     };
+  },
+  methods: {
+    slide: () => {
+      console.log(this.isSlideOut);
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  /* Buttons */
 
-  .btnRandom {
-    -moz-transition: all, ease-in-out 0.5s;
-    -o-transition: all, ease-in-out 0.5s;
-    -webkit-transition: all, ease-in-out 0.5s;
-    transition: all, ease-in-out 0.5s;
-  }
-
-  .btnRandom {
-    color: #114b8e;
-    font-size: 1em;
-    text-transform: uppercase;
-    text-decoration: none;
-    padding: 1em 1.6em;
-    margin: 3%;
-    border: none;
-    background: none;
-    position: relative;
-    display: inline-block;
-    letter-spacing: .0875em;
-    font-weight: 500;
-    border-radius: 100px;
-    outline: 0;
-  }
-
-
-  .btnRandom:hover {
-    color: #fff;
-    background: #de4f4f;
-    text-decoration: none;
-    outline: 0;
-  }
-
-  .z-depth-1-half,
-  .btnRandom {
-    box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.14), 0 1px 7px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -1px rgba(0, 0, 0, 0.2);
-  }
-
-  .z-depth-3,
-  .btnRandom:hover {
-    box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12), 0 3px 5px -1px rgba(0, 0, 0, 0.3);
+  .hide-projects {
+    opacity: 0;
   }
 
 </style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,80 @@
+<template>
+
+    <div class="background">
+      <div class="multicolor">
+        <div class="container">
+          <div id="app">
+            {{msg}}
+            <router-link to="/"><button class="btnRandom">Go Back</button></router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'projects',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      isSlideOut: false,
+      isSpin: true,
+    };
+  },
+  methods: {
+    slide: () => {
+      console.log(this.isSlideOut);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  /* Buttons */
+
+  .btnRandom {
+    -moz-transition: all, ease-in-out 0.5s;
+    -o-transition: all, ease-in-out 0.5s;
+    -webkit-transition: all, ease-in-out 0.5s;
+    transition: all, ease-in-out 0.5s;
+  }
+
+  .btnRandom {
+    color: #114b8e;
+    font-size: 1em;
+    text-transform: uppercase;
+    text-decoration: none;
+    padding: 1em 1.6em;
+    margin: 3%;
+    border: none;
+    background: none;
+    position: relative;
+    display: inline-block;
+    letter-spacing: .0875em;
+    font-weight: 500;
+    border-radius: 100px;
+    outline: 0;
+  }
+
+
+  .btnRandom:hover {
+    color: #fff;
+    background: #de4f4f;
+    text-decoration: none;
+    outline: 0;
+  }
+
+  .z-depth-1-half,
+  .btnRandom {
+    box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.14), 0 1px 7px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -1px rgba(0, 0, 0, 0.2);
+  }
+
+  .z-depth-3,
+  .btnRandom:hover {
+    box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12), 0 3px 5px -1px rgba(0, 0, 0, 0.3);
+  }
+
+</style>
